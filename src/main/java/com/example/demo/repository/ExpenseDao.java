86a23d384c7765a2a32cec19fb69af96353fbe84
unknown <unknown@DESKTOP-JT868C9>
2023-02-19 14:04:21 +0900

    first!

---
+++ src/main/java/com/example/demo/repository/ExpenseDao.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.User;
+import com.example.demo.repository.IExpenseDao;
+import org.springframework.jbdc;
+
+/**
+ * 情報 Repository
+ */
+@Repository
+public class ExpenseDao implements IExpenseDao {
+
+    private final NamedParmeter
+}
