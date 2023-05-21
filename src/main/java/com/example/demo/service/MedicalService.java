c268ccfc159b2f33695fc85b6e6e20d3a049b70b
MITSU-1216 <adieuer._.03.26@icloud.com>
2023-05-22 07:06:03 +0900

    list

--- src/main/java/com/example/demo/service/MedicalService.java
+++ src/main/java/com/example/demo/service/MedicalService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.repository.IMedicalDao;
 import com.example.demo.model.ModelMedical;
 
-// import java.util.List;
+import java.util.List;
 
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,6 +22,15 @@ public class MedicalService {
 
     public int resisterMedical(ModelMedical medical) {
         return dao.resisterMedical(medical);
+    }
+
+    /**
+     * ユーザー情報 全検索
+     * 
+     * @return 検索結果
+     */
+    public List<Medical> searchAll() {
+        return userRepository.findAll();
     }
 
 }
